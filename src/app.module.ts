d720b559718efec6a2ba6f2be5fd3660d27e370d
julienbrg <julien@strat.cc>
2024-10-21 17:38:04 +0200

    add config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AiModule } from './ai/ai.module';
 import { Web3Module } from './web3/web3.module';
 
 @Module({
-  imports: [AiModule, Web3Module],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    AiModule,
+    Web3Module,
+  ],
   controllers: [],
   providers: [],
 })
