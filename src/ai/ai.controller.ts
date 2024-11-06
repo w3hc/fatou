d0809781ffd0c7fafc3f9697a5a12993f7d3de3b
julienbrg <julien@strat.cc>
2024-11-06 15:32:28 +0100

    clean up

--- src/ai/ai.controller.ts
+++ src/ai/ai.controller.ts
@@ -53,7 +53,7 @@ export class AiController {
         cb(null, true);
       },
       limits: {
-        fileSize: 5 * 1024 * 1024, // 5MB limit
+        fileSize: 5 * 1024 * 1024,
       },
     }),
   )
