bc4fb134e4f3e89d151ef8023dd094a79f344fa0
julienbrg <julien@strat.cc>
2025-02-05 15:36:49 +0100

    allow any origin

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:3001', 'https://ouf.netlify.app'],
+    origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'x-api-key'],
   });
